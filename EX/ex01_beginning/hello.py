41e5a82af27504bae1afd92454622f378b305e24
jaaunt <jaaunt@taltech.ee>
2024-09-04 22:10:17 +0300

    huh

---
+++ EX/ex01_beginning/hello.py
@@ -0,0 +1,7 @@
+"""EX01 hello"""
+
+name = input("What is your name? ")
+num1 = int(input("Hello" + name + "! Enter a random number: "))
+num2 = int(input("Now enter a second number: "))
+sum = num1 + num2
+print(str(num1) + " + " + str(num2) + " is " + str(sum))
