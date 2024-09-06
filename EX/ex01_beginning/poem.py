c86475959deb0b75a6aa82a75e95d83c5f90affa
jaaunt <jaaunt@taltech.ee>
2024-09-06 13:49:50 +0300

    huh

--- EX/ex01_beginning/poem.py
+++ EX/ex01_beginning/poem.py
@@ -3,4 +3,4 @@
 colour = input("Give me a colour: ")
 objects = input("Give me a plural noun: ")
 activity = input("Give me an activity: ")
-print(f"Roses are {colour},\n{objects}are blue,\nI love to {activity}\nAnd so will you!")
+print(f"Roses are {colour},\n{objects} are blue,\nI love to {activity}\nAnd so will you!")
