46c071af4944cfedbb8f2d5f8b7bdd97218dfb25
jaaunt <jaaunt@taltech.ee>
2024-09-06 13:51:26 +0300

    huh

--- EX/ex01_beginning/atm.py
+++ EX/ex01_beginning/atm.py
@@ -13,12 +13,12 @@ We use four banknotes to cover it. The banknotes are 20€, 50€, 1€ and 1€
 
 amount = int(input("Enter a sum: "))
 banknotes = 0
-note_options = [100, 50, 20, 10, 5, 1] # hulk et saaks kasutada for funktsiooni ja labi vaadata koik need alustades suurimast
+note_options = [100, 50, 20, 10, 5, 1]  # hulk et saaks kasutada for funktsiooni ja labi vaadata koik need alustades suurimast
 for i in note_options:
     if amount == 0:
         break # lopetab ara kui summa on 0
-    how_manyfit = amount // i # jagab suurima taisarvuni, jagab kogusumma hulga liikmega (i)
-    banknotes += how_manyfit # lisab leitud koguse voi jatab samaks kui rahataht on liiga suur et seda anda
-    # pikalt banknotes = how_manyfit + banknotes (molemad tootavad sama moodi)
-    amount -= how_manyfit*i # lahutab algsummast selle palju rahatahtedes valja anti juba
+    how_manyfit = amount // i  # jagab suurima taisarvuni, jagab kogusumma hulga liikmega (i)
+    banknotes += how_manyfit  # lisab leitud koguse voi jatab samaks kui rahataht on liiga suur et seda anda
+      # pikalt banknotes = how_manyfit + banknotes (molemad tootavad sama moodi)
+    amount -= how_manyfit*i  # lahutab algsummast selle palju rahatahtedes valja anti juba
 print(f"Amount of banknotes needed: {banknotes}")
