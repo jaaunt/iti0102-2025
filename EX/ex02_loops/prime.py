6ea706857ac29da8a24766f396b9ac3f2371bb72
jaaunt <jaaunt@taltech.ee>
2024-09-15 17:11:20 +0300

    huh

--- EX/ex02_loops/prime.py
+++ EX/ex02_loops/prime.py
@@ -24,10 +24,8 @@ def is_prime_number(number: int) -> bool:
     for n in range(2, number):
         if number % n == 0:  # kontrollib kas jagub millegani nii et jaaki ei tekiks aka ei teki koma arvu
             return False
-        else:  # kui ei jagunud nii millegiga ss on prime number
-            return True
 
-
+    return True
 
 if __name__ == '__main__':
     print(is_prime_number(2))  # -> True
