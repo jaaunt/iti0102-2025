f138b736317cbd899608adc7bfb6262d648f5ae6
jaaunt <jaaunt@taltech.ee>
2024-09-17 22:03:19 +0300

    huh

--- EX/ex03_validation/password.py
+++ EX/ex03_validation/password.py
@@ -179,7 +179,7 @@ def is_password_valid(new_password: str, old_password: str, name: str, birthdate
             includes_number(new_password) and
             includes_special(new_password) and
             is_different_from_old_password(old_password, new_password)):
-        if not (is_name_in_password(new_password, name) and is_birthday_in_password(new_password, birthdate)):
+        if not is_name_in_password(new_password, name) and not is_birthday_in_password(new_password, birthdate):
             return True
     return False
 
