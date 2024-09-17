2e3b44cf24e6dccc1d7f03d017aee36ded6e642b
jaaunt <jaaunt@taltech.ee>
2024-09-17 22:00:28 +0300

    huh

--- EX/ex03_validation/password.py
+++ EX/ex03_validation/password.py
@@ -174,11 +174,11 @@ def is_password_valid(new_password: str, old_password: str, name: str, birthdate
     :return: True if the password is valid, False otherwise.
     """
     if (is_correct_length(new_password) and
-        includes_uppercase(new_password) and
-        includes_lowercase(new_password) and
-        includes_number(new_password) and
-        includes_special(new_password) and
-        is_different_from_old_password(old_password, new_password) is True):
+            includes_uppercase(new_password) and
+            includes_lowercase(new_password) and
+            includes_number(new_password) and
+            includes_special(new_password) and
+            is_different_from_old_password(old_password, new_password) is True):
         if (is_name_in_password(new_password, name) and is_birthday_in_password(new_password, birthdate)) is False:
             return True
     return False
