9745d767b903f50b93a72706928a1aea0ed05b20
jaaunt <jaaunt@taltech.ee>
2024-10-06 12:45:44 +0300

    huh

--- PROJECT/project1/phone_numbers.py
+++ PROJECT/project1/phone_numbers.py
@@ -79,6 +79,8 @@ def correct_numbers(numbers: list[str]) -> list[str]:
 def get_names_of_contacts_with_correct_numbers(names: list[str], numbers: list[str]) -> list[str]:
     """Get the names of contacts with correct numbers."""
     name_valid_nr = []
+    if len(names) != len(numbers):
+        raise ValueError("Names and numbers must have the same length.")
     for i in range(0, len(numbers)):
         if is_valid(numbers[i]):
             name_valid_nr.append(names[i])
