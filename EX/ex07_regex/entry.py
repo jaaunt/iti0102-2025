5e088ba3b37d846fb91e34a647c8d2c139ea5daa
jaaunt <jaaunt@taltech.ee>
2024-10-21 20:08:17 +0300

    huh

--- EX/ex07_regex/entry.py
+++ EX/ex07_regex/entry.py
@@ -21,10 +21,8 @@ def parse(row: str) -> tuple:
     """
     name_pattern = r"([A-Z][a-z]+)([A-Z][a-z]+)"  # first and last name
     id_pattern = r"(\d{11})"  # id kood 11 nr
-    phone_pattern = (r"(\+\d{3}"  # area kood
-                     r"\s*"  # tuhik nende vahel
-                     r"\d{7,8})")  # nr osa
-    date_pattern = r"\d{2}-\d{2}-\d{4}"
+    phone_pattern = (r"(\+\d{3}\s*\d{7,8}|\d{7,8})")
+    date_pattern = r"(\d{2}-\d{2}-\d{4})"
 
     # otsib koiki neid str-ist
     name_match = re.search(name_pattern, row)  # find all kuna ees ja pere nimi
