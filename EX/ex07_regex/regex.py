ad91b6c3f695e46e81bec8f0f0b7d109543384ca
jaaunt <jaaunt@taltech.ee>
2024-10-21 17:38:27 +0300

    huh

--- EX/ex07_regex/regex.py
+++ EX/ex07_regex/regex.py
@@ -16,7 +16,7 @@ def find_words(text: str) -> list:
      find words from
     :return: list of words found in given string
     """
-    pattern = r"[A-Z][a-zõäöü]+"
+    pattern = r"[A-ZÕÄÖÜ][a-zõäöü]+"
 
     words = re.findall(pattern, text)
 
