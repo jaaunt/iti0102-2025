5ddc0c69b6ed34ab81ff78b34be7b068cd831656
jaaunt <jaaunt@taltech.ee>
2024-10-28 17:05:01 +0200

    huh

--- EX/ex08_recursion/recursion.py
+++ EX/ex08_recursion/recursion.py
@@ -239,13 +239,9 @@ def sum_squares(nested_list: list | int) -> int:
     if isinstance(nested_list, int):
         return nested_list ** 2
     elif isinstance(nested_list, list):
-        if not nested_list:  # Handle the case of an empty list
+        if not nested_list:
             return 0
-
-        total = 0
-        for thing in nested_list:
-            total += sum_squares(thing)
-        return total
+        return sum_squares(nested_list[0]) + sum_squares(nested_list[1:])
     else:
         return 0
 
