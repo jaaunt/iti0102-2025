cb851648d3e86d73add0379e0bf6b38b7a0c95aa
jaaunt <jaaunt@taltech.ee>
2024-12-02 17:31:10 +0200

    huh

---
+++ EX/ex13_football/football.py
@@ -0,0 +1,285 @@
+"""Football."""
+
+
+class Player:
+    """Player class."""
+
+    def __init__(self, name: str, player_number: int):
+        """
+        Initialize player class.
+
+        :param name: The name of the player.
+        :param player_number: The number of the player.
+        """
+        self.name = name
+        self.player_number = player_number
+        self.goals_scored = 0
+        self.red_cards = 0
+
+    def __repr__(self) -> str:
+        """
+        Represent player.
+
+        Format the string of the player as:
+        '[name] ([player_number])'
+
+        :return: The string representation of the player.
+        """
+        return f'{self.name} ({self.player_number})'
+
+    def get_player_name(self) -> str:
+        """
+        Get player name.
+
+        :return: The player name.
+        """
+        return self.name
+
+    def get_player_number(self) -> int:
+        """
+        Get player number.
+
+        :return: The player number.
+        """
+        return self.player_number
+
+    def get_goals_scored(self) -> int:
+        """
+        Get amount of goals scored by player.
+
+        :return: The goals scored.
+        """
+        return self.goals_scored
+
+    def get_red_cards(self) -> int:
+        """
+        Get amount of red cards given to player.
+
+        :return: Amount of red cards.
+        """
+        return self.red_cards
+
+
+
+class Team:
+    """Team class."""
+
+    def __init__(self, name: str):
+        """
+        Initialize team.
+
+        Each team should have a name and can have up to 11 players.
+
+        :param name: The name of the team.
+        """
+        self.name = name
+        self.players = []
+
+    def __repr__(self) -> str:
+        """
+        Represent team.
+
+        Format the string of the team as:
+        '[name]'
+
+        :return: The string representation of the team.
+        """
+        return f'[{self.name}]'
+
+    def is_full(self) -> bool:
+        """
+        Check if team is full.
+
+        :return: The method should return True if the team is full, else False.
+        """
+        return len(self.players) == 11
+
+    def add_player(self, player: Player) -> bool:
+        """
+        Add a player to the team.
+
+        A player can only be added if the team has less than 11 players.
+        The same player cannot be added twice.
+        The method should return True if the player was added, else False.
+
+        :param player: The player to add.
+        :return: True if the player was added, else False.
+        """
+        if len(self.players) < 11:
+            self.players.append(player)
+            return True
+        return False
+
+    def remove_player(self, player: Player) -> bool:
+        """
+        Remove a player from the team.
+
+        The method should return True if the player was removed, else False.
+
+        :param player: The player to remove.
+        :return: True if the player was removed, else False.
+        """
+        pass
+
+    def get_team_name(self) -> str:
+        """
+        Get team name.
+
+        :return: The team name.
+        """
+        pass
+
+    def get_player_by_number(self, player_number: int) -> Player | None:
+        """
+        Get player with matching player number.
+
+        :param player_number: The player number to check for.
+        :return: Player object if found, None if not found.
+        """
+        pass
+
+    def get_players(self) -> list[Player]:
+        """
+        Return a list with all the players in the team.
+
+        :return: Team players as a list.
+        """
+        pass
+
+    def get_players_sorted(self) -> list[Player]:
+        """
+        Return a sorted list with all the players in the team.
+
+        The sorting order goes like this:
+        1. Most goals scored
+        2. Least red cards gotten
+        If both parameters are equal, then order randomly.
+
+        :return: Team players as a sorted list.
+        """
+        pass
+
+
+class Match:
+    """Match class."""
+
+    def __init__(self, team1: Team, team2: Team):
+        """
+        Initialize match.
+
+        A match consists of two teams and a score for each team.
+
+        :param team1: The first team.
+        :param team2: The second team.
+        """
+        pass
+
+    def player_scored(self, team: Team, player: Player) -> bool:
+        """
+        Increment score and keep records of the goalscorer.
+
+        The method should return True if the score was set, else False.
+
+        :param team: The team of the player who scored.
+        :param player: The player who scored.
+        :return: True if the score was set, else False.
+        """
+        pass
+
+    def give_red_card(self, player: Player) -> bool:
+        """
+        Give a red card to a player.
+
+        The player should be prevented from scoring for the rest of the game.
+        The method should return True if the red card was given, else False.
+
+        :param player: The player to give the red card to.
+        :return: True if the red card was given, else False.
+        """
+        pass
+
+    def get_score(self, team: Team) -> int:
+        """
+        Return the score of the given team.
+
+        :param team: The team whose score to return.
+        :return: The score of the given team.
+        """
+        pass
+
+    def get_winner(self) -> Team | None:
+        """
+        Get the winner of the match.
+
+        Return the team with the higher score.
+        If the scores are tied, return None.
+
+        :return: The team with the higher score, or None if tied.
+        """
+        pass
+
+    def get_top_goalscorer(self) -> Player:
+        """
+        Get the player with most scored goals in the given game.
+
+        Return the player with the most scored goals.
+
+        :return: The player with the most scored goals.
+        """
+
+    def has_red_card(self, player: Player) -> bool:
+        """
+        Check if a player has received a red card.
+
+        :param player: The player to check.
+        :return: True if the player has a red card, False otherwise.
+        """
+        pass
+
+    def get_red_carded_players(self) -> list[Player]:
+        """
+        Get the list of players who received red cards.
+
+        :return: List of players with red cards.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    """Main for testing the functions."""
+    # Initialize teams
+    team1 = Team("Team A")
+    team2 = Team("Team B")
+    print(team1)  # Team A
+    print(team2)  # Team B
+    print()
+
+    # Initialize players
+    player1 = Player("Alice", 1)
+    player2 = Player("Bob", 2)
+    player3 = Player("Charlie", 3)
+    print(player1)  # Alice (1)
+    print(player3)  # Charlie (3)
+    print()
+
+    # Add players to teams
+    print(team1.add_player(player1))  # True
+    print(team1.add_player(player1))  # False (no duplicates allowed)
+    print(team1.add_player(player2))  # True
+    print(team2.add_player(player3))  # True
+    print()
+
+    # Check players in teams
+    print(player1 in team1.get_players())  # True
+    print(player3 in team1.get_players())  # False
+    print(team1.get_players())  # [Alice (1), Bob (2)]
+    print()
+
+    # Initialize match
+    match = Match(team1, team2)
+
+    print(match.player_scored(team1, player1))  # True
+    print(match.player_scored(team2, player1))  # False (wrong team)
+    print(match.get_score(team1))  # 1
+    print(match.get_score(team2))  # 0
+    print(match.get_winner())  # Team A
